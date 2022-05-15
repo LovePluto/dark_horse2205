cf525046c8f505481de29671de4e975eeba2a37d
Pluto <591712730@qq.com>
2022-05-15 18:04:55 +0800

    [story1|AC2|工序8]：完成了异常处理

--- src/main/java/com/wyh/dark_horse/infrastructure/exception/GlobalException.java
+++ src/main/java/com/wyh/dark_horse/infrastructure/exception/GlobalException.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @ControllerAdvice
@@ -18,5 +19,12 @@ public class GlobalException {
     public ErrorBody handlerException(Exception e) {
         log.error("unknown error!", e);
         return ErrorBody.ERROR;
+    }
+
+    @ExceptionHandler(MarginException.class)
+    @ResponseStatus(code = CONFLICT)
+    public ErrorBody handlerMarginException(MarginException e) {
+        log.error("margin failed!", e);
+        return new ErrorBody(4001, e.getMessage());
     }
 }
---
+++ src/main/java/com/wyh/dark_horse/infrastructure/exception/MarginException.java
@@ -0,0 +1,7 @@
+package com.wyh.dark_horse.infrastructure.exception;
+
+public class MarginException extends RuntimeException {
+    public MarginException(String message) {
+        super(message);
+    }
+}
