58223e625c8ae812d76cd8b43e731a4304917193
Pluto <591712730@qq.com>
2022-05-15 18:04:14 +0800

    [story1|AC2|工序1]：完成了缴纳保证金失败业务处理

--- src/main/java/com/wyh/dark_horse/margin/MarginService.java
+++ src/main/java/com/wyh/dark_horse/margin/MarginService.java
@@ -1,5 +1,6 @@
 package com.wyh.dark_horse.margin;
 
+import com.wyh.dark_horse.infrastructure.exception.MarginException;
 import com.wyh.dark_horse.margin.client.PaymentClient;
 import com.wyh.dark_horse.margin.model.Margin;
 import com.wyh.dark_horse.margin.model.MarginResult;
@@ -20,6 +21,9 @@ public class MarginService {
         MarginResult result = paymentClient.payment(margin);
         margin.update(result);
         marginRepository.save(margin);
+        if (!result.getResult()) {
+            throw new MarginException("账户余额不足，请确认账户是否有足够余额缴纳保证金！");
+        }
         return margin.getId();
     }
 }
